b73fc2d8e23ffc04f7395f385cee9a821f11e948
Juliusz Pawlus <60894769+Bladdyy@users.noreply.github.com>
2025-03-08 10:53:22 +0100

    Update projekt1.hs

--- projekt1.hs
+++ projekt1.hs
@@ -1,7 +1,12 @@
+-- Definition of Expr.
+
 data Expr = S | K | I | B 
           | Expr :$ Expr 
           | X | Z | V Int  
           deriving (Show, Read)
+
+
+-- Test Expr cases.
 
 test1 = S :$ K :$ K :$ X
 twoB = S :$B :$ I
@@ -11,10 +16,13 @@ omega = ((S :$ I) :$ I) :$ ((S :$ I) :$ I)
 kio = K :$ I :$ omega
 add = (B :$ S) :$ (B :$ B)
 
+
+-- Turning expr to string.
+
 prettyExpr expr = remove(simple expr)
     where 
-        simple (a :$ (b :$ c)) = simple a ++ " (" ++ remove (simple (b :$ c)) ++ ")"
-        simple (a :$ b) = simple a ++ simple b
+        simple (a :$ (b :$ c)) = simple a ++ " (" ++ remove (simple (b :$ c)) ++ ")"  -- If right Expr is in brackets it should stay in it.
+        simple (a :$ b) = simple a ++ simple b  -- Simplify both Expr.
         simple (V a) = " v" ++ show a
         simple (X) = " x"
         simple (Z) = " z"
@@ -22,28 +30,48 @@ prettyExpr expr = remove(simple expr)
         simple (K) = " K"
         simple (I) = " I"
         simple (B) = " B"
-        remove (x:xs) = xs
+        remove (x:xs) = xs  -- Removing redundant whitespace from the beginning of the bracket.
 
-rstep expr = reduce Nothing [expr] (dig expr)
+
+-- Performs one reduction step. Pref is a saved prefix which can't be reduced. Expr is expression left to reduce.
+
+rstep pref expr = reduce pref expr
     where
+    reduce pref (S:a:b:c:ts) = (pref, (a:c:(b:$c):ts))
+    reduce pref (K:a:b:ts) = (pref, (a:ts))
+    reduce pref (I:a:ts) = (pref, (a:ts))
+    reduce pref (B:a:b:c:ts) = (pref, (a:(b:$c):ts))
+    reduce pref ((a :$ b):ts) = reduce pref (a:b:ts)      -- If front of expression is in bracktes, then unpacking.
+    
+    reduce Nothing (h:ts) = reduce (Just h) ts            -- If there is no match in patterns above, then front of the expression is not a redex.
+    reduce (Just ex) (h:ts) = reduce (Just (ex :$ h)) ts  -- If there was some prefix before.
+    reduce _ [] = (Nothing, [])  -- If there is no more expression to reduce.
+
+
+-- Performes max n steps of reduction. 
+
+rpath expr = steps Nothing (dig expr) [prettyExpr expr] 0
+    where
+    steps pref expr list count = 
+        let (n_pref, seqe) = rstep pref expr  -- Perform a step
+        in 
+        if not (null seqe) && count < 30      -- If number of steps is not exceeded and there is a sequence to reduce.
+        then steps n_pref seqe ((normalize n_pref seqe) : list) (count + 1)  
+        else rev list []  -- Return reversed list of steps.
+
+    -- Changes List to Expr.
+    normalize pref (h : m : ts) = normalize pref ((h :$ m) : ts)        -- Change rest of the exspression to Expr.
+    normalize (Just (a :$ b)) (h : ts) = normalize (Just a) [b :$ (h)]  -- Add whole prefix.
+    normalize (Just a) (h : ts) = prettyExpr (a :$ h)
+    normalize Nothing (h : ts) = prettyExpr h
+
+    -- Changes Expr to List
     dig (h :$ t) = ((dig h) ++ [t])
     dig h = [h]
-    reduce pref steps (S:a:b:c:ts) = reduce pref (normalize pref (a:c:(b:$c):ts) : steps) (a:c:(b:$c):ts)
-    reduce pref steps (K:a:b:ts) = reduce pref (normalize pref (a:ts) : steps) (a:ts)
-    reduce pref steps (I:a:ts) = reduce pref (normalize pref (a:ts) : steps) (a:ts)
-    reduce pref steps (B:a:b:c:ts) = reduce pref (normalize pref (a:(b:$c):ts) : steps) (a:(b:$c):ts)
-    reduce pref steps ( (a :$ b):ts ) = reduce pref steps (a:b:ts)
-    reduce Nothing steps (h:ts) = reduce (Just h) steps ts
-    reduce (Just ex) steps (h:ts) = reduce (Just (ex :$ h)) steps ts  -- Tutaj nawiasy na odwrót prefiksu.
-    reduce _ steps [] = rev_pretty [] steps
 
-    normalize pref (h : m : ts) = normalize pref ((h :$ m) : ts)
-    normalize (Just (a :$ b)) (h : ts) = normalize (Just a) [b :$ (h)]
-    normalize (Just a) (h : ts) = (a :$ h)
-    normalize Nothing (h : ts) = h
+    -- Reverts the list.
+    rev (h:ts) out = rev ts (h:out) 
+    rev [] out = out
+ 
 
-    rev_pretty els (h : ts) = rev_pretty ((prettyExpr h) : els) ts
-    rev_pretty els [] = els 
-
-
-printPath expr = mapM_ putStrLn (rstep expr) 
+printPath expr = mapM_ putStrLn (rpath expr) 
